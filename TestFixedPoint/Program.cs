b82fa954e9664566ec48bed7ef92efe0de296ae8
Michal Medvecky <miskomedvecky@gmail.com>
2020-03-17 11:01:33 +0100

    Added requested source code

--- TestFixedPoint/Program.cs
+++ TestFixedPoint/Program.cs
@@ -1,44 +1,112 @@
-﻿using System;
+﻿#define STUDENT_VERSION
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace TestFixedPoint
+using Cuni.Arithmetics.FixedPoint;
+
+namespace FixedPointApiTest
 {
-    class Program
-    {
-        static string FixedPointPrinter(long input, int point)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(input >> point);
-            for (int i = 0; ((input & ((1 << point) - 1)) > 0) && (i < point); i++)
-            {
-                if (i == 0)
-                    sb.Append('.');
-                input &= (1 << point) - 1;
-                input *= 10;
-                sb.Append(input >> point);
-            }
-            return sb.ToString();
-        }
-        class A
-        {
-            public static A Instance => new A();
-            A()
-            {
-            }
-            public void PrintMe()
-            {
-                Console.WriteLine("hello from singleton A");
+#if STUDENT_VERSION
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Q24_8:");
+			{
+				var f1 = new Fixed<Q24_8>(3);
+				Console.WriteLine($"3: {f1}");
 
-            }
-        }
-        static void Main(string[] args)
-        {
-            long a = 13;
-            int point = 2;
-            Console.WriteLine(FixedPointPrinter(a, point));
+				var f2 = new Fixed<Q24_8>(2);
+				var f3 = f1.Add(f2);
+				Console.WriteLine($"3 + 2: {f3}");
 
-            A.Instance.PrintMe();
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"3 * 2: {f3}");
 
-        }
-    }
+				f1 = new Fixed<Q24_8>(19);
+				f2 = new Fixed<Q24_8>(13);
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"19 * 13: {f3}");
+
+				f1 = new Fixed<Q24_8>(3);
+				f2 = new Fixed<Q24_8>(2);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"3 / 2: {f3}");
+
+				f1 = new Fixed<Q24_8>(248);
+				f2 = new Fixed<Q24_8>(10);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"248 / 10: {f3}");
+
+				f1 = new Fixed<Q24_8>(625);
+				f2 = new Fixed<Q24_8>(1000);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"625 / 1000: {f3}");
+			}
+
+			//
+
+			Console.WriteLine();
+			Console.WriteLine("Q16_16:");
+			{
+				var f1 = new Fixed<Q16_16>(3);
+				Console.WriteLine($"3: {f1}");
+
+				var f2 = new Fixed<Q16_16>(2);
+				var f3 = f1.Add(f2);
+				Console.WriteLine($"3 + 2: {f3}");
+
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"3 * 2: {f3}");
+
+				f1 = new Fixed<Q16_16>(19);
+				f2 = new Fixed<Q16_16>(13);
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"19 * 13: {f3}");
+
+				f1 = new Fixed<Q16_16>(248);
+				f2 = new Fixed<Q16_16>(10);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"248 / 10: {f3}");
+
+				f1 = new Fixed<Q16_16>(625);
+				f2 = new Fixed<Q16_16>(1000);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"625 / 1000: {f3}");
+			}
+
+			//
+
+			Console.WriteLine();
+			Console.WriteLine("Q8_24:");
+			{
+				var f1 = new Fixed<Q8_24>(3);
+				var f2 = new Fixed<Q8_24>(2);
+				var f3 = f1.Add(f2);
+				Console.WriteLine($"3 + 2: {f3}");
+
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"3 * 2: {f3}");
+
+				f1 = new Fixed<Q8_24>(19);
+				f2 = new Fixed<Q8_24>(13);
+				f3 = f1.Multiply(f2);
+				Console.WriteLine($"19 * 13: {f3}");
+
+				f1 = new Fixed<Q8_24>(248);
+				f2 = new Fixed<Q8_24>(10);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"248 / 10: {f3}");
+
+				f1 = new Fixed<Q8_24>(625);
+				f2 = new Fixed<Q8_24>(1000);
+				f3 = f1.Divide(f2);
+				Console.WriteLine($"625 / 1000: {f3}");
+			}
+		}
+	}
+#endif
 }
