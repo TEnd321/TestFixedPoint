08a08d5c674431db80f84216829f2bfa1362ac84
Michal Medvecky <miskomedvecky@gmail.com>
2020-03-17 11:00:19 +0100

    Add project files.

---
+++ FixedPointArith/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public interface IFixed
+    {
+        int Point { get; }
+        long Number { get; }
+    }
+    public interface IFixedArith
+    {
+
+    }
+    public sealed class Printer
+    {
+        public static Printer Instance => new Printer();
+        Printer() { }
+        public string Print(long input, int point)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(input >> point);
+            for (int i = 0; ((input & ((1 << point) - 1)) > 0) && (i < point); i++)
+            {
+                if (i == 0)
+                    sb.Append('.');
+                input &= (1 << point) - 1;
+                input *= 10;
+                sb.Append(input >> point);
+            }
+            return sb.ToString();
+        }
+    }
+    public class Fixed
+    {
+
+    }
+}
---
+++ TestFixedPoint/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace TestFixedPoint
+{
+    class Program
+    {
+        static string FixedPointPrinter(long input, int point)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(input >> point);
+            for (int i = 0; ((input & ((1 << point) - 1)) > 0) && (i < point); i++)
+            {
+                if (i == 0)
+                    sb.Append('.');
+                input &= (1 << point) - 1;
+                input *= 10;
+                sb.Append(input >> point);
+            }
+            return sb.ToString();
+        }
+        class A
+        {
+            public static A Instance => new A();
+            A()
+            {
+            }
+            public void PrintMe()
+            {
+                Console.WriteLine("hello from singleton A");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            long a = 13;
+            int point = 2;
+            Console.WriteLine(FixedPointPrinter(a, point));
+
+            A.Instance.PrintMe();
+
+        }
+    }
+}
