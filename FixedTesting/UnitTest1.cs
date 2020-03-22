738f10daeab704704af5c7378150dc72d640fa1e
Michal Medvecky <miskomedvecky@gmail.com>
2020-03-22 11:18:10 +0100

    Added new tests.

--- FixedTesting/UnitTest1.cs
+++ FixedTesting/UnitTest1.cs
@@ -4,7 +4,7 @@ using Cuni.Arithmetics.FixedPoint;
 
 namespace FixedTesting
 {
-    public class UnitTest1
+    public class UnitQ16_16
     {
         [Theory]
         [InlineData(3, 2, "5")]
@@ -55,6 +55,35 @@ namespace FixedTesting
     }
     public class UnitQ24_8
     {
+        [Theory]
+        [InlineData(3, 2, "5")]
+        [InlineData(3, 0, "3")]
+        public void AdditionTestingSameTypesQ24_8(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q24_8>(in1);
+            var f2 = new Fixed<Q24_8>(in2);
+            Assert.Equal(exp, f1.Add(f2).ToString());
+        }
+
+        [Theory]
+        [InlineData(360, 42, "318")]
+        [InlineData(14, 12, "2")]
+        public void SubtractionTestingSameTypesQ24_8(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q24_8>(in1);
+            var f2 = new Fixed<Q24_8>(in2);
+            Assert.Equal(exp, f1.Subtract(f2).ToString());
+        }
+
+        [Theory]
+        [InlineData(3, 2, "6")]
+        [InlineData(19, 13, "247")]
+        public void MultiplicationTestingSameTypesQ24_8(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q24_8>(in1);
+            var f2 = new Fixed<Q24_8>(in2);
+            Assert.Equal(exp, f1.Multiply(f2).ToString());
+        }
 
         [Theory]
         [InlineData(248, 10, "24.796875")]
@@ -71,14 +100,55 @@ namespace FixedTesting
     public class UnitQ8_24
     {
         [Theory]
+        [InlineData(3, 2, "5")]
+        [InlineData(3, 0, "3")]
+        public void AdditionTestingSameTypesQ8_24(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q8_24>(in1);
+            var f2 = new Fixed<Q8_24>(in2);
+            Assert.Equal(exp, f1.Add(f2).ToString());
+        }
+
+        [Theory]
+        [InlineData(360, 42, "62")]
+        [InlineData(25, 12, "13")]
+        public void SubtractionTestingSameTypesQ8_24(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q8_24>(in1);
+            var f2 = new Fixed<Q8_24>(in2);
+            Assert.Equal(exp, f1.Subtract(f2).ToString());
+        }
+
+        [Theory]
+        [InlineData(3, 2, "6")]
+        [InlineData(19, 13, "-9")]
+        public void MultiplicationTestingSameTypesQ8_24(int in1, int in2, string exp)
+        {
+            var f1 = new Fixed<Q8_24>(in1);
+            var f2 = new Fixed<Q8_24>(in2);
+            Assert.Equal(exp, f1.Multiply(f2).ToString());
+        }
+
+        [Theory]
         [InlineData(248, 10, "-0.799999952316284")]
         [InlineData(625, 1000, "-4.70833331346512")]
-        //[InlineData(14, 12, "26")]
         public void DivisionTestingSameTypesQ8_24(int in1, int in2, string exp)
         {
             var f1 = new Fixed<Q8_24>(in1);
             var f2 = new Fixed<Q8_24>(in2);
             Assert.Equal(exp, f1.Divide(f2).ToString());
+        }
+    }
+    public class UnitTestingMixed
+    {
+        [Fact]
+        public void TestTwoDistinct()
+        {
+            Fixed<Q16_16> f1 = new Fixed<Q16_16>(15);
+            Fixed<Q24_8> f2 = new Fixed<Q24_8>(15);
+            //Build will fail with error code CS1503: Cannot convert from Fixed<Q24_8> to Fixed<Q16_16>
+            //Console.WriteLine(f1.Add(f2));
+            Assert.True(true);
         }
     }
 }
